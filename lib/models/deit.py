c626737d8b213bf8bd67c7c0f434ca957e1ab4fe
Haotian Bai <164140757@qq.com>
2022-02-06 14:51:43 +0800

    fist commitment

---
+++ lib/models/deit.py
@@ -0,0 +1,235 @@
+from einops import rearrange, repeat
+import torch
+from torch.functional import einsum
+import torch.nn as nn
+from functools import partial
+
+from .graphFusion import Fuse
+
+
+from .vision_transformer import VisionTransformer, _cfg
+from timm.models.registry import register_model
+
+__all__ = [
+    'deit_tscam_tiny_patch16_224', 'deit_tscam_small_patch16_224', 'deit_tscam_base_patch16_224', 'vit_tscam_base_patch16_224'
+]
+
+def embeddings_to_cosine_similarity_matrix(tokens) :
+    """
+    Shapes for inputs:
+    - tokens : :math:`(B, N, D)` where B is the batch size, N is the target `spatial` sequence length, D is the token representation length.
+    
+    Shapes for outputs:
+    
+    Converts a a tensor of D embeddings to an (N, N) tensor of similarities.
+    """
+    dot = torch.einsum('bij, bkj -> bik', [tokens, tokens])
+    norm = torch.norm(tokens, p=2, dim=-1) 
+    x = torch.div(dot, torch.einsum('bi, bj -> bij', norm, norm))
+    
+    return x
+
+class TSCAM(VisionTransformer):
+    def __init__(self, num_layers=4, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.head = nn.Linear(self.embed_dim, self.num_classes)
+        self.head = nn.Conv2d(self.embed_dim, self.num_classes, kernel_size=3, stride=1, padding=1)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.head.apply(self._init_weights)
+        self.num_layers = num_layers
+        self.layers = nn.ModuleList()
+        for i in range(self.num_layers):
+            self.layers.append(Encoder(dim=self.num_classes))                
+
+    def forward_features(self, x):
+        # taken from https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py
+        # with slight modifications to return patch embedding outputs
+        B = x.shape[0]
+        x = self.patch_embed(x)
+
+        cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
+        x = torch.cat((cls_tokens, x), dim=1)
+        x = x + self.pos_embed
+        x = self.pos_drop(x)
+        attn_weights = []
+        for blk in self.blocks:
+            x, weights = blk(x)
+            attn_weights.append(weights)
+
+        x = self.norm(x)
+        return x[:, 0], x[:, 1:], attn_weights
+
+    def forward(self, x, return_cam=False):
+        x_cls, x_patch, attn_weights = self.forward_features(x)
+        n, p, c = x_patch.shape
+        
+        x_patch = torch.reshape(x_patch, [n, int(p**0.5), int(p**0.5), c])
+        x_patch = x_patch.permute([0, 3, 1, 2])
+        x_patch = x_patch.contiguous()
+        x_patch = self.head(x_patch)
+        # x_patch = self.head(rearrange(x_patch, 'B D H W -> B H W D'))
+        # x_patch = rearrange(x_patch, 'B H W D -> B D H W')
+        pred_box = x_patch
+        
+        attn_weights = torch.stack(attn_weights)        # 12 * B * H * N * N
+        attn_weights = torch.mean(attn_weights, dim=2)  # 12 * B * N * N
+
+        n, c, h, w = x_patch.shape
+        cams = attn_weights.sum(0)[:, 0, 1:].reshape([n, h, w]).unsqueeze(1)
+        cams = rearrange(cams, 'n 1 h w -> n h w')
+        cams = norm_cam(cams)
+        cams = rearrange(cams, 'n h w -> n (h w)')
+        pred_cam = rearrange(cams, 'B (H W) -> B 1 H W', H=h)
+        
+        for i, layer in enumerate(self.layers):
+            x_patch, cams = layer(x_patch, cams)
+            
+        # x_patch = self.head(x_patch)
+        # pred_box = x_patch
+        # pred_cam = rearrange(cams, 'B (H W) -> B 1 H W', H=h)
+        x_logits = self.avgpool(x_patch).squeeze(3).squeeze(2)
+        
+        if self.training:
+            return x_logits
+        else:
+            return x_logits, pred_box*pred_cam
+
+class Encoder(nn.Module):
+    def __init__(self,
+                 dim,
+                 thred=0.5,
+                 residual=True,
+                 fusion_cfg=dict(loss_rate=1, grid_size=(14, 14), iteration=4),
+                 ) -> None:
+        super().__init__()
+        self.fuse = Fuse(**fusion_cfg)
+        self.shrink = nn.Tanhshrink()
+        self.thred = nn.Parameter(torch.ones([1])*thred)
+        self.residual = residual
+        H, W = fusion_cfg['grid_size']
+        # self.norm = nn.LayerNorm([dim, H, W])
+
+    def forward(self, x, cam):
+        """foward function given x and spt
+
+        Args:
+            x (torch.Tensor): patch tokens, tensor of shape [B D H W]
+            cam (torch.Tensor): cam values, tensor of shape [B N]
+        Returns:
+            x (torch.Tensor): patch tokens, tensor of shape [B D H W]
+            cam (torch.Tensor): cam values, tensor of shape [B N]
+        """
+        sim = embeddings_to_cosine_similarity_matrix(rearrange(x, 'B D H W -> B (H W) D'))
+        thred = self.thred.to(x.device)
+        out_cam = einsum('b h w, b w -> b h', self.fuse(sim), cam)
+        thred = thred * cam.max(1, keepdim=True)[0]
+        out_cam = self.shrink(out_cam/thred)
+        out_cam = norm_cam(out_cam)
+        out_x = einsum('b d h w, b h w -> b d h w', x, rearrange(out_cam, 'B (H W) -> B H W', H=x.shape[-2]))
+        # out_x = self.norm(out_x)
+        if self.residual:
+            x = x + out_x
+            cam = cam + out_cam
+        return x, cam
+    
+def norm_cam(cam):
+    # cam [B N]
+    if len(cam.shape) == 3:
+        cam = cam - repeat(rearrange(cam, 'B H W -> B (H W)').min(1, keepdim=True)[0], 'B 1 -> B 1 1')
+        cam = cam / repeat(rearrange(cam, 'B H W -> B (H W)').max(1, keepdim=True)[0], 'B 1 -> B 1 1')
+    elif len(cam.shape) == 2:
+        cam = cam - cam.min(1, keepdim=True)[0]
+        cam = cam / cam.max(1, keepdim=True)[0]       
+        
+    return cam
+
+@register_model
+def deit_tscam_tiny_patch16_224(pretrained=False, **kwargs):
+    model = TSCAM(
+        patch_size=16, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained:
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url="https://dl.fbaipublicfiles.com/deit/deit_tiny_patch16_224-a1311bcf.pth",
+            map_location="cpu", check_hash=True
+        )['model']
+        model_dict = model.state_dict()
+
+        for k in ['head.weight', 'head.bias', 'head_dist.weight', 'head_dist.bias']:
+            if k in checkpoint and checkpoint[k].shape != model_dict[k].shape:
+                print(f"Removing key {k} from pretrained checkpoint")
+                del checkpoint[k]
+
+        pretrained_dict = {k: v for k, v in checkpoint.items() if k in model_dict}
+        model_dict.update(pretrained_dict)
+        model.load_state_dict(model_dict)
+    return model
+
+@register_model
+def deit_tscam_small_patch16_224(pretrained=False, **kwargs):
+    model = TSCAM(
+        patch_size=16, embed_dim=384, depth=12, num_heads=6, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained:
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url="https://dl.fbaipublicfiles.com/deit/deit_small_patch16_224-cd65a155.pth",
+            map_location="cpu", check_hash=True
+        )['model']
+        model_dict = model.state_dict()
+        for k in ['head.weight', 'head.bias', 'head_dist.weight', 'head_dist.bias']:
+            if k in checkpoint and checkpoint[k].shape != model_dict[k].shape:
+                print(f"Removing key {k} from pretrained checkpoint")
+                del checkpoint[k]
+        pretrained_dict = {k: v for k, v in checkpoint.items() if k in model_dict}
+        model_dict.update(pretrained_dict)
+        model.load_state_dict(model_dict)
+    return model
+
+
+@register_model
+def deit_tscam_base_patch16_224(pretrained=False, **kwargs):
+    model = TSCAM(
+        patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained:
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url="https://dl.fbaipublicfiles.com/deit/deit_base_patch16_224-b5f2ef4d.pth",
+            map_location="cpu", check_hash=True
+        )['model']
+        model_dict = model.state_dict()
+        for k in ['head.weight', 'head.bias', 'head_dist.weight', 'head_dist.bias']:
+            if k in checkpoint and checkpoint[k].shape != model_dict[k].shape:
+                print(f"Removing key {k} from pretrained checkpoint")
+                del checkpoint[k]
+        pretrained_dict = {k: v for k, v in checkpoint.items() if k in model_dict}
+        model_dict.update(pretrained_dict)
+        model.load_state_dict(model_dict)
+    return model
+@register_model
+def vit_tscam_small_patch16_224(pretrained=False, **kwargs):
+    model = TSCAM(
+        patch_size=16, embed_dim=768, depth=8, num_heads=8, mlp_ratio=3, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained:
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url='https://storage.googleapis.com/vit_models/augreg/'
+            'S_16-i21k-300ep-lr_0.001-aug_light1-wd_0.03-do_0.0-sd_0.0--imagenet2012-steps_20k-lr_0.03-res_224.npz',
+            map_location="cpu", check_hash=True
+        )
+        model_dict = model.state_dict()
+        for k in ['head.weight', 'head.bias', 'head_dist.weight', 'head_dist.bias']:
+            if k in checkpoint and checkpoint[k].shape != model_dict[k].shape:
+                print(f"Removing key {k} from pretrained checkpoint")
+                del checkpoint[k]
+        pretrained_dict = {k: v for k, v in checkpoint.items() if k in model_dict}
+        model_dict.update(pretrained_dict)
+        model.load_state_dict(model_dict)
+    return model
+
+
+
+
---
+++ val_ilsvrc.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH -J fViT
+#SBATCH -p p-V100
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+
+cd /home/baihaotian/programs/TS-CAM
+source /cm/shared/apps/anaconda3/etc/profile.d/conda.sh
+conda activate cu102
+# conda activate cu113
+
+module load cuda11.2/toolkit/11.2.2
+module load cuda11.2/fft/11.2.2
+module load cuda11.2/blas/11.2.2
+
+GPU_ID=0
+NET='deit'
+NET_SCALE='small'
+SIZE='224'
+
+WORK_DIR="/mntnfs/med_data2/haotian/work_dirs/"
+# PATH_='ckpt/CUB/model/deit_tiny/ckpt/model_best_top1_loc.pth'
+# PATH_='ckpt/CUB/model/conformer_small/ckpt/model_best_top1_loc.pth'
+# PATH_='ckpt/CUB/cls_repre/ly4/ckpt/model_best_top1_loc.pth'
+PATH_='ckpt/ImageNet/ly8/deit_small/ckpt/model_epoch20.pth'
+WORK_DIR=${WORK_DIR}$(echo ${PATH_})
+export CUDA_VISIBLE_DEVICES=${GPU_ID}
+
+python ./tools_cam/test_cam.py --config_file configs/ILSVRC/${NET}_tscam_${NET_SCALE}_patch16_${SIZE}.yaml --resume ${WORK_DIR} TEST.SAVE_BOXED_IMAGE True MODEL.CAM_THR 0.1
